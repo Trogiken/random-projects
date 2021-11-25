684a591e08ab53749a86eb4b3e1a6fdf81165c9d
Trogiken <noah@blaszak.com>
2021-11-24 23:56:34 -0500

    Documentation and Planning
    Requirements and Rule, along with version documentation added

---
+++ python/projects/hangman/main.py
@@ -0,0 +1,10 @@
+"""
+REQUIREMENTS
+Store a modifiable list of words to be used
+Windows and Linux terminal compatibility
+
+RULES
+* A number of times a user can get the incorrect letter and display tries left
+* Display correct and incorrect letters separately
+*
+"""
