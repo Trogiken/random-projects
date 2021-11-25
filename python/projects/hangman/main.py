00ba1ef032a2e2f632821bb4369229e07275c4c1
Trogiken <noah@blaszak.com>
2021-11-25 00:30:13 -0500

    test

--- python/projects/hangman/main.py
+++ python/projects/hangman/main.py
@@ -2,24 +2,28 @@
 REQUIREMENTS
 Store a modifiable list of words to be used
 Windows and Linux terminal compatibility
+Save Scores and Display before each game
 
 RULES
 * A number of times a user can get the incorrect letter and display tries left
 * Display correct and incorrect letters separately
 """
 
-import sys
 from termcolor import colored, cprint
 
 text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
 print(text)
 cprint('Hello, World!', 'green', 'on_red')
 
-print_red_on_cyan = lambda x: cprint(x, 'red', 'on_cyan')
+
+def print_red_on_cyan(x):
+    cprint(x, 'red', 'on_cyan')
+
+
 print_red_on_cyan('Hello, World!')
 print_red_on_cyan('Hello, Universe!')
 
 for i in range(10):
     cprint(i, 'magenta', end=' ')
 
-cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
+cprint("Attention!", 'red', attrs=['bold'])
