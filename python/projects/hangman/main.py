08ca262f9a0f92a8b30a6dcd12cfca93227d8157
Trogiken <noah@blaszak.com>
2021-11-25 00:12:30 -0500

    Test

--- python/projects/hangman/main.py
+++ python/projects/hangman/main.py
@@ -6,5 +6,20 @@ Windows and Linux terminal compatibility
 RULES
 * A number of times a user can get the incorrect letter and display tries left
 * Display correct and incorrect letters separately
-*
 """
+
+import sys
+from termcolor import colored, cprint
+
+text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
+print(text)
+cprint('Hello, World!', 'green', 'on_red')
+
+print_red_on_cyan = lambda x: cprint(x, 'red', 'on_cyan')
+print_red_on_cyan('Hello, World!')
+print_red_on_cyan('Hello, Universe!')
+
+for i in range(10):
+    cprint(i, 'magenta', end=' ')
+
+cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
