a9a49ffba14265a78a8bdc4096c7584718eef11c
Trogiken <noah@blaszak.com>
2021-11-27 18:01:21 -0500

    Comments Cleanup

--- python/projects/hangman/assets/process_word.py
+++ python/projects/hangman/assets/process_word.py
@@ -6,12 +6,11 @@ from .os_prop import *
 def fetch_words():
     """Read all words in words.txt and return in list format"""
     cwd = os.getcwd()
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     words = []
     with open(cwd + f"{get_os(1)}words", 'r') as f:
         raw_words = f.read().splitlines()
         for item in raw_words:
-            if item:  # If not empty
+            if item:
                 for char in item:
                     if char.isalpha():
                         continue
@@ -19,7 +18,7 @@ def fetch_words():
                         input(f"Letters Only! - [{item}]\nPress Enter to Exit")
                         exit()
                 words.append(item)
-        if words:  # If not empty
+        if words:
             return words
         else:
             print("Words list Empty!")
