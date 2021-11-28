89329f7fac199b97d44af61d623571166df0caab
Trogiken <noah@blaszak.com>
2021-11-28 15:30:01 -0500

    Version Check
    Python 3.8+ is checked for when program is launched

--- python/projects/hangman/assets/os_info.py
+++ python/projects/hangman/assets/os_info.py
@@ -1,7 +1,20 @@
 import sys
 
+# Python 3.8 Version Check
+major = sys.version_info.major
+minor = sys.version_info.minor
+micro = sys.version_info.micro
+current = f"{major}.{minor}.{micro}"
+if major >= 3 and minor >= 8:
+    print("Pass")
+    pass
+else:
+    print(f"[Incorrect Python Version]\n"
+          f"Requires: Python 3.8+ (Current - {current})")
+
 
 def get_os(opt):
+    """Gather OS dependant information"""
     if sys.platform == "win32":
         path_form = "\\"
         system = "Windows"
