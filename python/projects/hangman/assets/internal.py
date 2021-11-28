de8fb1d97f140ab5f57e2834b66ae5682538f8fe
Trogiken <noah@blaszak.com>
2021-11-28 18:36:03 -0500

    YAML Support

--- python/projects/hangman/assets/internal.py
+++ python/projects/hangman/assets/internal.py
@@ -1,3 +1,24 @@
+from yaml import safe_load
+
+
+def config(key):
+    with open('yaml.yaml', 'r') as file:
+        prime_service = safe_load(file)
+
+    options = ['url', 'port', 'prime']
+    config_variable = None
+    if key not in options:
+        return config_variable
+    elif key == 'url':
+        config_variable = prime_service['rest']['url']
+    elif key == 'port':
+        config_variable = prime_service['rest']['port']
+    elif key == 'prime':
+        config_variable = prime_service['prime_numbers']
+
+    return config_variable
+
+
 class Game:
     used_words = []
 
--- python/projects/hangman/hangman.py
+++ python/projects/hangman/hangman.py
@@ -7,6 +7,7 @@ def main():
     os.system(get_os(3))
     # Print Random Word
     print(get_word())
+    print(config('port'))
 
 
 while __name__ == '__main__':
