c5dc389b3f42d1a54798949c3081d839740bd94c
Trogiken <noah@blaszak.com>
2023-06-15 01:05:57 -0400

    even better animals

--- python/projects/ClassAnimals/file.py
+++ python/projects/ClassAnimals/file.py
@@ -1,69 +1,130 @@
 import pickle
+import os
+import hashlib
+
 
 class Animal:
+    """A class representing an animal"""
     def __init__(self, legs=None, colors=None):
+        """Initialize the Animal instance"""
         self.legs = legs
         self.colors = colors
 
-    def save(self, filename):
-        with open(filename, 'wb') as f:
-            pickle.dump(self, f)
+    def __str__(self):
+        """Return a string representation of the instance"""
+        return f"legs: {self.legs}, colors: {self.colors}"
 
-    @staticmethod
-    def load(filename):
-        with open(filename, 'rb') as f:
-            return pickle.load(f)
+    def __repr__(self):
+        """Return a string representation of the instance"""
+        return f"Animal(legs={self.legs}, colors={self.colors})"
 
 
 class Cow(Animal):
+    """A class representing a cow"""
     def __init__(self, legs=None, colors=None):
+        """Initialize the Cow instance"""
         default_legs = 4
         default_colors = ['Brown', 'White', 'Spotted']
         super().__init__(legs or default_legs, colors or default_colors)
 
 
 class Pig(Animal):
+    """A class representing a pig"""
     def __init__(self, legs=None, colors=None):
+        """Initialize the Pig instance"""
         default_legs = 4
         default_colors = ['Pink', 'Black', 'Spotted']
         super().__init__(legs or default_legs, colors or default_colors)
 
 
 class Chicken(Animal):
+    """A class representing a chicken"""
     def __init__(self, legs=None, colors=None):
+        """Initialize the Chicken instance"""
         default_legs = 2
         default_colors = ['White']
         super().__init__(legs or default_legs, colors or default_colors)
 
 
+def save_animals(filename, **animals):
+    """Save all animal instances into a single file"""
+    animal_data = {}
+    for name, animal in animals.items():
+        # Store the animal instance in the dictionary with the name as the key
+        animal_data[name] = animal
+    with open(filename, 'wb') as f:
+        pickle.dump(animal_data, f)
+
+    # Calculate the hash of the serialized data
+    serialized_data = pickle.dumps(animal_data)
+    hash_value = hashlib.sha256(serialized_data).hexdigest()
+    # Store the hash value in the dictionary
+    animal_data['__hash__'] = hash_value
+    # Save the animal data again with the hash
+    with open(filename, 'wb') as f:
+        pickle.dump(animal_data, f)
+
+
+def load_animals(filename):
+    """Load all animal instances from a single file"""
+    with open(filename, 'rb') as f:
+        loaded_data = pickle.load(f)
+
+    # Retrieve the hash value and remove it from the dictionary
+    hash_value = loaded_data.pop('__hash__', None)
+    # Calculate the hash of the serialized data by loading the data again
+    serialized_data = pickle.dumps(loaded_data)
+    calculated_hash = hashlib.sha256(serialized_data).hexdigest()
+
+    print('Saved Hash:', hash_value)
+    print('Calculated Hash:', calculated_hash)
+
+    # This verifies if data is tampered, not the authenticity of the class code itself!
+    if hash_value != calculated_hash:
+        print('Data has been tampered with!')
+    else:
+        print('Data is intact!')
+
+    return loaded_data
+
+
 def calculate_total_legs(animals):
+    """Calculate the total number of legs"""
     total_legs = 0
     for animal in animals:
-        if animal.legs is not None:
-            total_legs += animal.legs
+        total_legs += animal.legs
     return total_legs
 
 
 # Create instances of the animals with custom attributes
-cow = Cow(legs=4, colors=['Brown', 'White'])
-pig = Pig(colors=['Pink', 'Black'])
-chicken = Chicken(legs=2)
+cow = Cow()
+pig = Pig()
+chicken = Chicken()
 
-# Save each animal instance
-cow.save('cow.pkl')
-pig.save('pig.pkl')
-chicken.save('chicken.pkl')
+# Save all animal instances into a single file with the file being in the same directory as this file
+save_animals(os.path.join(os.path.dirname(__file__), 'animal_data.pkl'), cow=cow, pig=pig, chicken=chicken)
+# Load the animal instances from the file
+loaded_animals = load_animals(os.path.join(os.path.dirname(__file__), 'animal_data.pkl'))
 
-# Load the animal instances
-loaded_cow = Cow.load('cow.pkl')
-loaded_pig = Pig.load('pig.pkl')
-loaded_chicken = Chicken.load('chicken.pkl')
+# Access the loaded animal instances by their names
+loaded_cow = loaded_animals['cow']
+loaded_pig = loaded_animals['pig']
+loaded_chicken = loaded_animals['chicken']
 
-print("loaded_cow legs:", loaded_cow.legs)
-print("loaded_pig legs:", loaded_pig.legs)
-print("loaded_chicken legs:", loaded_chicken.legs)
+# Example of using __str__ and __repr__
+print('loaded_cow:', loaded_cow)
+# Example of using __eq__ and __ne__
+print('loaded_cow == loaded_pig:', loaded_cow == loaded_pig)\
+
+print()
+
+# Example of using __getattr__
+print('loaded_cow legs:', loaded_cow.legs)
+print('loaded_pig legs:', loaded_pig.legs)
+print('loaded_chicken legs:', loaded_chicken.legs)
+print('-' * 25)
 
 # Calculate the total number of legs
 animals = [loaded_cow, loaded_pig, loaded_chicken]
 total_legs = calculate_total_legs(animals)
-print("Total number of legs:", total_legs)
+print("Total number of legs:", total_legs, '\n')
