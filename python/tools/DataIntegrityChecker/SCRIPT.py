f7100ddf62fb88600efcf539c9b7a872756e9bda
Trogiken <noah@blaszak.com>
2023-06-28 12:56:36 -0400

    Major speed improvements * Implimented batches and pools * Store already checked files and compare that for checking new files * Removed RAM option because python is slow

--- python/tools/DataIntegrityChecker/SCRIPT.py
+++ python/tools/DataIntegrityChecker/SCRIPT.py
@@ -3,9 +3,9 @@
 import os
 import sys
 import hashlib
-import json
-import shelve
 import sqlite3
+from typing import Tuple
+from multiprocessing import Pool
 from PyQt6.QtWidgets import QApplication, QFileDialog
 
 
@@ -35,10 +35,15 @@ def ask_directory(title: str) -> str:
         sys.exit()
 
 
-def create_hash(file_path: str) -> str:
+def create_hash(file_path: str) -> Tuple[str, str]:
     """Create hash from file bytes using the chunk method, return hash as a string if found."""
     try:
-        chunk_size = 4096  # TODO Adjust the chunk size as per requirements (1 Gb in size use 8096)
+        chunk_size = 4096
+        file_size = os.path.getsize(file_path)
+        
+        if file_size > 1_000_000_000:  # If file size around 1 Gb or larger
+            chunk_size = 8192
+
         hasher = hashlib.sha256()
         
         with open(file_path, 'rb') as file:
@@ -49,102 +54,25 @@ def create_hash(file_path: str) -> str:
                 hasher.update(chunk)
         
         file_hash = hasher.hexdigest()
-        return file_hash
+        return file_path, file_hash
     except BaseException as error:
-        # if error is keyboard interrupt, exit program
-        if isinstance(error, KeyboardInterrupt):
-            print('Keyboard Interrupt Detected')
-            sys.exit()
         print(f"Failed to create hash! | {error}\nReplacing hash with '?'")
-        return '?'
+        return file_path, '?'
 
-def create_hash_dict_memory(directory_path: str) -> dict:
-    """Utilizing the disk, walk through a directory and subdirectories to create a dictionary of hashes for each file, return dictionary."""
-    hash_dict = {'attributes': {'working_directory': directory_path},
-                 'hashes': {}}  # create dictionary with working directory as attribute
 
-    print('\nCreating Hash Dictionary...\n')
-    hashes = hash_dict['hashes']
-    for root, _, files in os.walk(directory_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            calculated_hash = create_hash(file_path)
-            # add file path and file hash to dictionary
-            hashes[file_path] = calculated_hash
-            print(f"'{file_path}' -> '{calculated_hash}'")
-    
-    return hash_dict
-
-def check_data_integrity_memory(directory_path: str, hashes: dict) -> dict:
-    """Utilizing Memory, Walk through a directory and subdirectories to check for data integrity, return a summary of the results."""
-    print('\nChecking for Data Integrity...\n')
-
-    number_new_files = 0
-    number_deleted_files = 0
-    number_bad_files = 0
-    number_ok_files = 0
-    number_unknown_files = 0
-
-    files_checked = False
-    for file_path in hashes:
-        files_checked = True
-        file_hash = hashes[file_path]
-        calculated_hash = create_hash(file_path)
-
-        if not os.path.exists(file_path):
-            print(f"'{file_path}' -> '{file_hash}' : Deleted")
-            number_deleted_files += 1
-            continue
-        
-        if file_hash == '?':
-            print(f"'{file_path}' : ?")
-            number_unknown_files += 1
-        elif file_hash == calculated_hash:
-            print(f"'{file_path}' -> '{file_hash}' : Ok")
-            number_ok_files += 1
+def create_hash_db(directory_path: str, data_save_path: str) -> int:
+    if os.path.exists(data_save_path):
+        print(f"\nHash database already exists at '{data_save_path}'\nDelete it? (y/n)\n")
+        delete_database = input('Enter option: ')
+        if delete_database.casefold() == 'y':
+            os.remove(data_save_path)
         else:
-            print(f"'{file_path}' -> '{file_hash}' != '{calculated_hash}' : Bad")
-            number_bad_files += 1
-    if not files_checked:
-        print('No files checked')
+            print('Canceled')
+            sys.exit()
     
-    print('\nChecking for new files...\n')
-    new_files = False
-    for root, _, files in os.walk(directory_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            calculated_hash = create_hash(file_path)
-
-            if file_path not in hashes:
-                new_files = True
-                print(f"'{file_path}' -> '{calculated_hash}' : New File")
-                number_new_files += 1
-    if not new_files:
-        print('No new files found')
-
-
-    return {'number_new_files': number_new_files, 'number_deleted_files': number_deleted_files, 
-            'number_bad_files': number_bad_files, 'number_ok_files': number_ok_files, 'number_unknown_files': number_unknown_files}
-
-# TODO now that hash function is fixed with chunks this may be able to be used
-# def create_hash_dict_disk(directory_path: str, dict_save_path: str) -> int:
-#     """Utilizing the disk, walk through a directory and subdirectories to create a dictionary of hashes for each file, return number of files."""
-#     with shelve.open(dict_save_path) as shelf:
-#         shelf['attributes'] = {'working_directory': directory_path, 'function': 'disk'}
-#         shelf['hashes'] = {}
-#         for root, _, files in os.walk(directory_path):
-#             for file in files:
-#                 file_path = os.path.join(root, file)
-#                 calculated_hash = create_hash(file_path)
-#                 # add file path and file hash to dictionary
-#                 shelf['hashes'][file_path] = calculated_hash
-#                 print(f"'{file_path}' -> '{calculated_hash}'")
-    
-#     return len(shelf['hashes'])
-def create_hash_dict_disk(directory_path: str, data_save_path: str) -> int:
     connection = sqlite3.connect(data_save_path)
     cursor = connection.cursor()
-    
+
     # Create table for hashes
     cursor.execute('''CREATE TABLE IF NOT EXISTS hashes (
                         file_path TEXT PRIMARY KEY,
@@ -158,13 +86,27 @@ def create_hash_dict_disk(directory_path: str, data_save_path: str) -> int:
     # Insert working directory into the table
     cursor.execute('INSERT OR REPLACE INTO attributes (working_directory) VALUES (?)', (directory_path,))
 
-    for root, _, files in os.walk(directory_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            calculated_hash = create_hash(file_path)
-            # Insert file path and hash into the table
-            cursor.execute('INSERT OR REPLACE INTO hashes (file_path, calculated_hash) VALUES (?, ?)', (file_path, calculated_hash))
-            print(f"'{file_path}' -> '{calculated_hash}'")
+    # Batch size for parameterized queries
+    batch_size = 1000
+    batch_data = []
+
+    print('\nCreating Hashes...\n')
+
+    with Pool() as pool:
+        for root, _, files in os.walk(directory_path):
+            file_paths = [os.path.join(root, file) for file in files]
+            results = pool.map(create_hash, file_paths)
+            batch_data.extend(results)
+
+            if len(batch_data) >= batch_size:  # If batch size is reached, insert data into the database
+                cursor.executemany('INSERT OR REPLACE INTO hashes (file_path, calculated_hash) VALUES (?, ?)', batch_data)
+                batch_data = []
+                print(f"Processed {len(results)} files")
+
+        if batch_data:  # If there are any remaining files to be inserted
+            cursor.executemany('INSERT OR REPLACE INTO hashes (file_path, calculated_hash) VALUES (?, ?)', batch_data)
+            print(f"Processed {len(batch_data)} files")
+
     connection.commit()
     connection.close()
 
@@ -176,8 +118,10 @@ def create_hash_dict_disk(directory_path: str, data_save_path: str) -> int:
 
     return num_files
 
-def check_data_integrity_disk(directory_path: str, data_save_path: str) -> dict:
-    """Utilizing the disk, walk through a directory and subdirectories to check for data integrity, return a summary of the results."""
+
+def check_data_integrity_db(directory_path: str, data_save_path: str) -> dict:
+    """Utilizing the hash database, check data integrity of files in a directory and subdirectories,
+    and return a summary of the results."""
     connection = sqlite3.connect(data_save_path)
     cursor = connection.cursor()
 
@@ -189,18 +133,19 @@ def check_data_integrity_disk(directory_path: str, data_save_path: str) -> dict:
     number_ok_files = 0
     number_unknown_files = 0
 
-    files_checked = False
+    files_checked = set()  # BUG: May cause memory issues if there are a lot of files
     for row in cursor.execute('SELECT file_path, calculated_hash FROM hashes'):
-        files_checked = True
+        files_checked.add(row[0])
         file_path = row[0]
         file_hash = row[1]
-        calculated_hash = create_hash(file_path)
 
         if not os.path.exists(file_path):
             print(f"'{file_path}' -> '{file_hash}' : Deleted")
             number_deleted_files += 1
             continue
-        
+
+        calculated_hash = create_hash(file_path)
+
         if file_hash == '?':
             print(f"'{file_path}' : ?")
             number_unknown_files += 1
@@ -210,29 +155,35 @@ def check_data_integrity_disk(directory_path: str, data_save_path: str) -> dict:
         else:
             print(f"'{file_path}' -> '{file_hash}' != '{calculated_hash}' : Bad")
             number_bad_files += 1
-    if not files_checked:
-        print('No files checked')
-    
+
     print('\nChecking for new files...\n')
+
     new_files = False
     for root, _, files in os.walk(directory_path):
         for file in files:
             file_path = os.path.join(root, file)
-            calculated_hash = create_hash(file_path)
-
-            cursor.execute('SELECT COUNT(*) FROM hashes WHERE file_path=?', (file_path,))
-            if cursor.fetchone()[0] == 0:
+            if file_path not in files_checked:
                 new_files = True
+                calculated_hash = create_hash(file_path)
                 print(f"'{file_path}' -> '{calculated_hash}' : New File")
                 number_new_files += 1
+
+    if not files_checked:
+        print('No files checked')
+
     if not new_files:
         print('No new files found')
 
     connection.close()
 
-    return {'number_new_files': number_new_files, 'number_deleted_files': number_deleted_files, 
-            'number_bad_files': number_bad_files, 'number_ok_files': number_ok_files, 'number_unknown_files': number_unknown_files}
-        
+    return {
+        'number_new_files': number_new_files,
+        'number_deleted_files': number_deleted_files,
+        'number_bad_files': number_bad_files,
+        'number_ok_files': number_ok_files,
+        'number_unknown_files': number_unknown_files
+    }
+
 
 
 if __name__ == '__main__':
@@ -240,60 +191,36 @@ if __name__ == '__main__':
 
     print("""
         Options:
-        1. Create Hash Dictionary
+        1. Create Hash Database
         2. Check Data Integrity
     """)
 
     main_option_selected = input('Enter option: ')
 
     if main_option_selected == '1':
-        print("""
-            Mode:
-            1. RAM - Faster, needs a lot of RAM (Recommended for small - medium amount of files)
-            2. Disk - Slower, uses disk read/write (Recommended for large amount of files)
-        """)
-        mode = input('Mode: ')
-
-        print('Select directory to start creating a hash dictionary')
+        print('Select directory to start creating a hash database')
         directory_path = ask_directory('Select a Start Directory')
-        print(f'Selecting directory: {directory_path}')
+        print(f'Selected hash directory: {directory_path}')
 
-        print('Select a location to store the hash dictionary')
+        print('Select a location to store the hash database')
         data_save_path = ask_directory('Select a Save Directory')
+        print(f'Selected save directory: {data_save_path}')
         
+        data_save_path = os.path.join(data_save_path, 'hash_data.sqlite3')
+        number_files = create_hash_db(directory_path, data_save_path)
 
-        if mode == '1':
-            data_save_path = os.path.join(data_save_path, 'hash_data.json')
-            hash_dict = create_hash_dict_memory(directory_path)
-            with open(data_save_path, 'w') as file:
-                json.dump(hash_dict, file, indent=4)
-                number_files = len(hash_dict['hashes'])
-        elif mode == '2':
-            data_save_path = os.path.join(data_save_path, 'hash_data.sqlite3')
-            number_files = create_hash_dict_disk(directory_path, data_save_path)
-        else:
-            print('Invalid mode selected')
-            sys.exit()
-
-        print(f"DONE!\n{number_files} Files Hashed\nHash dictionary saved to '{data_save_path}'")
+        print(f"DONE!\n{number_files} Files Hashed\nHash database saved to '{data_save_path}'")
     elif main_option_selected == '2':
-        print('Select Hash Dictionary')
+        print('Select Hash Database')
 
-        data_save_path = ask_filelocation('Select Hash Dictionary', 'JSON (*.json);;SQLite3 (*.sqlite3)')
+        data_save_path = ask_filelocation('Select Hash Database', 'SQLite3 (*.sqlite3)')
 
         # TODO Add a way to change the working directory path such as if the files were transfered to a new drive
 
         # get extension
         _, extension = os.path.splitext(data_save_path)
 
-        if extension == '.json':
-            with open(data_save_path, 'r') as file:
-                hash_dict = json.load(file)
-                hashes = hash_dict['hashes']
-                working_directory = hash_dict['attributes']['working_directory']
-            
-            data_summary = check_data_integrity_memory(working_directory, hashes)
-        elif extension == '.sqlite3':
+        if extension == '.sqlite3':
             connection = sqlite3.connect(data_save_path)
             cursor = connection.cursor()
             cursor.execute('SELECT calculated_hash FROM hashes')
@@ -302,9 +229,9 @@ if __name__ == '__main__':
             working_directory = cursor.fetchone()[0]
             connection.close()
 
-            data_summary = check_data_integrity_disk(working_directory, data_save_path)
+            data_summary = check_data_integrity_db(working_directory, data_save_path)
         else:
-            print('Invalid extension detected.')
+            print('Invalid File Detected')
             sys.exit()
         
         summary_msg = f"Summary [{working_directory}]"
