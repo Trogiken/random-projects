5f5266ab69e02da5b133bbe4369a9487217e4e31
Trogiken <noah@blaszak.com>
2023-07-04 12:43:05 -0400

    for loop

---
+++ C++/Learning/forLoops.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int nums[] = {1, 2, 5, 7, 9};
+    for (int i = 0; i < 5; i++) {  // (initialization; condition; increment/decrement)
+        std::cout << nums[i] << std::endl;
+    }
+
+    return 0;
+}
