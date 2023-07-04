5cfdf699b5b5d03fad8094946757f1dadc3f7d67
Trogiken <noah@blaszak.com>
2023-07-04 12:47:17 -0400

    for loops 2.0

--- C++/Learning/forLoops.cpp
+++ C++/Learning/forLoops.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
+int powerWithFor(int baseNum, int powNum) {
+    // 2^3 = 2 * 2 * 2
+    // Each time we loop, we multiply the result by the base number
+    int result = 1;
+    for (int i = 0; i < powNum; i++) {
+        result = result * baseNum;
+    }
+    return result;
+}
+
 int main() {
     int nums[] = {1, 2, 5, 7, 9};
     for (int i = 0; i < 5; i++) {  // (initialization; condition; increment/decrement)
         std::cout << nums[i] << std::endl;
     }
+
+    std::cout << "----------------" << std::endl;
+    std::cout << powerWithFor(2, 3) << std::endl;
 
     return 0;
 }
