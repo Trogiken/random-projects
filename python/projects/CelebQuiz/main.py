7545cfe962120cac391b8b50263c596b63f48396
Trogiken <noah@blaszak.com>
2023-07-11 14:10:33 -0400

    init

---
+++ python/projects/CelebQuiz/main.py
@@ -0,0 +1,13 @@
+"""
+print theme
+
+create a LIST of options for each question
+Use a for loop to print all the questions under the question - OPTIONAL
+store the user's answer in a variable for each question
+
+    print all the questions under the theme in sequence:
+        Question 1
+        for loop for question 1 options:
+
+use if statements to determine what celebrity the user is
+"""
