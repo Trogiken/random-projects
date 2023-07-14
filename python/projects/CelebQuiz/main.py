252e0971c3ead2e0029100c3d54de6cac42c76af
leah-chip <123521812+leah-chip@users.noreply.github.com>
2023-07-14 01:18:41 -0400

    Celeb quiz (#7)
    * init
    
    * i tried super hard
    
    * nonsense
    
    * more info
    
    * fgd
    
    * mv theme print
    
    * rm file
    
    * I DID IT WHOOHOOO!
    
    * Indent cleanup
    
    * deleted leahquiz
    
    ---------
    
    Co-authored-by: Trogiken <noah@blaszak.com>

---
+++ python/projects/CelebQuiz/main.py
@@ -0,0 +1,105 @@
+"""Celeb Quiz Program"""
+
+score = {
+    'harry': 0,
+    'taylor': 0,
+}
+
+questions_quiz = {
+    'question_one_answers': ["charismatic", "funny", "sweet", "generous"],
+    'question_two_answers': ["sleep", "read", "meditate", "dance"],  
+    'question_three_answers': ["hamster", "cat", "dog", "goldfish"],
+    'question_four_answers': ["blue", "yellow", "green", "purple"],
+}
+
+
+def question_one():
+    """Func for q1"""
+    q1 = questions_quiz.get('question_one_answers')
+
+    print("How would your friends describe you?\n")
+    for index, option in enumerate(q1):
+        index += 1
+        print(f"{index}. {option}")
+
+    answer = input('> ')
+
+    if answer in ['1', '2']:
+        print('One point for Harry')
+        score['harry'] += 1
+    else:
+        print('One point for Taylor')
+        score['taylor'] += 1
+
+
+def question_two():
+    """Function for q2"""
+    q2 = questions_quiz.get('question_two_answers')
+
+    print("What is your favorite thing to do at home?\n")
+    for index, option in enumerate(q2):
+        index += 1
+        print(f"{index}, {option}")
+
+    answer = input('> ')
+
+    if answer in ['3', '4']:
+        print('One point for Harry')
+        score['harry'] += 1
+    else:
+        print('One point for Taylor')
+        score['taylor'] += 1
+
+
+def question_three():
+    """Function for q3"""
+    q3 = questions_quiz.get('question_three_answers')
+
+    print("Which is your favorite animal?\n")
+    for index, option in enumerate(q3):
+        index += 1
+        print(f"{index}, {option}")
+
+    answer = input('> ')
+
+    if answer in ['3', '4']:
+        print('One point for Harry')
+        score['harry'] += 1
+    else:
+        print('One point for Taylor')
+        score['taylor'] += 1
+
+
+def question_four():
+    """Function for q4"""
+    q4 = questions_quiz.get('question_four_answers')
+
+    print("What is your favorite color?\n")
+    for index, option in enumerate(q4):
+        index += 1
+        print(f"{index}, {option}")
+
+    answer = input('> ')
+
+    if answer in ['1', '3']:
+        print('One point for Harry')
+        score['harry'] += 1
+    else:
+        print('One point for Taylor')
+        score['taylor'] += 1
+
+
+if __name__ == '__main__':
+    print("The celebrity you are is...\n")
+
+    question_one()
+    question_two()
+    question_three()
+    question_four()
+
+    if score['harry'] == score['taylor']:
+        print('It was a tie!')
+    elif score['harry'] >= score['taylor']:
+        print('You are Harry Styles!')
+    else:
+        print('You are Taylor Swift!')
