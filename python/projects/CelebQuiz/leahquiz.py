9a187dcaadc3aa515631af79b4fb4275013de92c
leah-chip <123521812+leah-chip@users.noreply.github.com>
2023-07-14 04:04:20 +0000

    I DID IT WHOOHOOO!

---
+++ python/projects/CelebQuiz/leahquiz.py
@@ -0,0 +1,37 @@
+print("Find which celebrity you are!")
+
+questions_quiz = [
+    question_one_answers = "charismatic, funny, sweet, generous",
+    question_two_answers = "sleep, read, meditate, dance",  
+    question_three_answers = "hamster, cat, dog, goldfish", 
+    question_four_answers = "blue, yellow, green, purple",
+]
+
+def question_one(input())
+return('How would your friends describe you?')
+
+if question_one == question_one_answers([0], [1]):
+    print('One point for Harry')
+else:
+print('One point for Taylor')
+
+
+def question_two(input())
+return('What is your favorite thing to do at home?')
+
+if question_two == question_two_answers([0], [2]):
+    print('One point for Harry')
+else:
+print('One point for Taylor')
+
+
+def question_three(input())
+return('Which is your favorite animal?')
+
+if question_three == question_three_answers([2], [3]):
+    print('One point for Harry')
+else:
+print('One point for Taylor')
+
+question_four = input()
+return
--- python/projects/CelebQuiz/main.py
+++ python/projects/CelebQuiz/main.py
@@ -18,63 +18,102 @@ score = {
 }
 
 questions_quiz = {
-    'question_one_answers': ["charismatic", "funny", "sweet", "generous"], # CORRECT
-    # question_two_answers = "sleep, read, meditate, dance",  
-    # question_three_answers = "hamster, cat, dog, goldfish",
-    # question_four_answers = "blue, yellow, green, purple",
+    'question_one_answers': ["charismatic", "funny", "sweet", "generous"],
+    'question_two_answers': ["sleep", "read", "meditate", "dance"],  
+    'question_three_answers': ["hamster", "cat", "dog", "goldfish"],
+    'question_four_answers': ["blue", "yellow", "green", "purple"],
 }
 
 
 def question_one():
     """Func for q1"""
-    q1 = questions_quiz.get('question_one_answers')
+q1 = questions_quiz.get('question_one_answers')
 
-    print("How would your friends describe you?\n")
-    for index, option in enumerate(q1):
-        index += 1
-        print(f"{index}. {option}")
+print("How would your friends describe you?\n")
+for index, option in enumerate(q1):
+    index += 1
+    print(f"{index}. {option}")
 
-    answer = input('> ')
+answer = input('> ')
 
-    if answer in ['1', '2']:
-        print('One point for Harry')
-        score['harry'] += 1
-    else:
-        print('One point for Taylor')
-        score['taylor'] += 1
-
-
-# def question_two(input())
-# return('What is your favorite thing to do at home?')
+if answer in ['1', '2']:
+    print('One point for Harry')
+    score['harry'] += 1
+else:
+    print('One point for Taylor')
+    score['taylor'] += 1
 
 
-# if question_two == question_two_answers[2]:
-#     print('One point for Harry')
-# else:
-#     print('One point for Taylor')
+def question_two():
+    """Function for q2"""
+q2 = questions_quiz.get('question_two_answers')
+
+print("What is your favorite thing to do at home?\n")
+for index, option in enumerate(q2):
+    index += 1
+    print(f"{index}, {option}")
+
+answer = input('> ')
+
+if answer in ['3', '4']:
+    print('One point for Harry')
+    score['harry'] += 1
+else:
+    print('One point for Taylor')
+    score['taylor'] += 1
 
 
+def question_three():
+    """Function for q3"""
+q3 = questions_quiz.get('question_three_answers')
+
+print("Which is your favorite animal?\n")
+for index, option in enumerate(q3):
+    index += 1
+    print(f"{index}, {option}")
+
+answer = input('> ')
+
+if answer in ['3', '4']:
+    print('One point for Harry')
+    score['harry'] += 1
+else:
+    print('One point for Taylor')
+    score['taylor'] += 1
 
 
-# def question_three(input())
-# return('Which is your favorite animal?')
+def question_four():
+    """Function for q4"""
+q4 = questions_quiz.get('question_four_answers')
 
+print("What is your favorite color?\n")
+for index, option in enumerate(q4):
+    index += 1
+    print(f"{index}, {option}")
 
-# if question_three == question_three_answers([2], [3]):
-#     print('One point for Harry')
-# else:
-#     print('One point for Taylor')
+answer = input('> ')
 
-
-# question_four = input()
+if answer in ['1', '3']:
+    print('One point for Harry')
+    score['harry'] += 1
+else:
+    print('One point for Taylor')
+    score['taylor'] += 1
 
 
 if __name__ == '__main__':
-    print("Find which celebrity you are!\n")
-    
+    print("The celebrity you are is...\n")
+
     question_one()
     question_two()
     question_three()
     question_four()
 
-    # use score to compare what celeb the user is
+if score['harry'] == score['taylor']:
+    print('It was a tie!')
+
+
+elif score['harry'] >= score['taylor']:
+    print('You are Harry Styles!')
+else:
+    print('You are Taylor Swift!')
