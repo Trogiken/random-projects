06fb9648495bf2d542c16c675c19afcc42274306
Trogiken <70231827+Trogiken@users.noreply.github.com>
2023-09-23 12:58:44 -0400

    f

--- python/tools/YoutubeToGo/source/misc.py
+++ python/tools/YoutubeToGo/source/misc.py
@@ -1,4 +1,4 @@
-def incomplete_function(func) -> function:
+def incomplete_function(func):
     """Decorator to mark a function as incomplete."""
     def wrapper(*args, **kwargs):
         raise NotImplementedError(f'{func.__name__} is not implemented')
