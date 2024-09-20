97b2a39bcb120cab31c18adb2114bd7c782f5997
Noah <70231827+Trogiken@users.noreply.github.com>
2024-09-20 17:58:30 -0400

    git setup

--- shell/kali-linux/setup.sh
+++ shell/kali-linux/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Make sure script is run as root
+if [ $EUID -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
 # Update kali
 echo
 echo "****Updating Kali****"
@@ -35,13 +41,6 @@ for i in "${list_menu[@]}"; do
     sudo apt install -y $i
 done
 
-# Install Tor Browser
-echo
-echo "****Installing Tor Browser****"
-echo
-sleep 2s
-sudo apt install -y tor
-
 # Update again
 echo
 echo "****Checking for Updates****"
@@ -57,30 +56,41 @@ echo
 sleep 2s
 sudo apt install -y git
 
-echo
-read -p "Enter your name for Git: " name
-read -p "Enter your email for Git: " email
-read -p "Enter your Personal Access Token for Git: " token
-
 # Configure Git
 echo
-echo "****Configuring Git****"
-echo
-sleep 2s
-git config --global credential.helper store
-git config --global user.name $name
-git config --global user.email $email
-git config --global user.token $token
+read -p "Would you like to configure Git? (y/n): " configure
+if [ $configure == "y" ]; then
+    echo
+    echo "****Configuring Git****"
+    echo
+    sleep 2s
+    echo
+    read -p "Enter your name for Git: " name
+    read -p "Enter your email for Git: " email
+    read -p "Enter your Personal Access Token for Git: " token
+    git config --global credential.helper store
+    git config --global user.name $name
+    git config --global user.email $email
+    git config --global user.token $token
+else
+    echo
+    echo "****Skipping Git Configuration****"
+    echo
+fi
 
 # Install Misc Tools
 echo
-echo "****Installing Misc Tools****"
+echo "****Installing Misc Tools and Packages****"
 echo
 sleep 2s
 sudo apt install -y gobuster
-git clone https://github.com/danielmiessler/SecLists.git
-git clone https://github.com/lgandx/Responder.git
-
+sudo apt install -y tor
+if [ -n "$(git config --global user.name)" ] && [ -n "$(git config --global user.email)" ]; then
+    git clone https://github.com/danielmiessler/SecLists.git
+    git clone https://github.com/lgandx/Responder.git
+else
+    echo "Git is not configured. Skipping cloning repositories."
+fi
 
 # Fix Installs
 echo
